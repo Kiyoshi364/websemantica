32f40de9967a307cd0f9f064191d428182d85cbf
Shikiyo <shikiyo@archjk>
2025-04-22 19:56:39 -0300

    turtle: sketch parsing rules

--- parsers/turtle.pl
+++ parsers/turtle.pl
@@ -11,6 +11,12 @@ matcheq_impl([], E, Cases) --> { throw(error(unreachable_match(E, Cases))) }.
 matcheq_impl([If_2-Then | Cs], E, Cases) -->
   if_(call(If_2, E), Then, matcheq_impl(Cs, E, Cases)).
 
+matcheq_expect(Elem, PredName, Cases) --> matcheq_expect_impl(Cases, Elem, PredName, _, _).
+
+matcheq_expect_impl([], E, PredName, Expected, []) --> { throw(error(expected_got_predicate(Expected, E, PredName))) }.
+matcheq_expect_impl([C-Then | Cs], E, PredName, Expected, [C | X]) -->
+  if_(C = E, Then, matcheq_expect_impl(Cs, E, PredName, Expected, X)).
+
 :- use_module(linecol, [char//2, unchar//2]).
 
 :- use_module(library(debug)).
@@ -35,17 +41,98 @@ comment(Comment) -->
 
 id(C0, C0) --> [].
 
-token(T, P) -->
-  char(C0, P0),
+token(T) -->
+  char(C0),
   matcheq(C0, [
-    ws_t      - token(T, P),
-    eof_t     - { T = eof, P = P0 },
-    comment_t - ( { T = comment(C), P = P0 }, comment(C) ),
-    comma_t   - { T = comma, P = P0 },
-    semi_t    - { T = semi, P = P0 },
-    dot_t     - { T = dot, P = P0 },
-    =(C0 )    - ( { T = id(ID), P = P0 }, id(C0, ID) )
+    ws_t      - token(T),
+    eof_t     - { T = eof },
+    comment_t - ( { T = comment(C) }, comment(C) ),
+    comma_t   - { T = comma },
+    semi_t    - { T = semi },
+    dot_t     - { T = dot },
+    =('[')    - { T = open_square },
+    =(']')    - { T = close_square },
+    =(C0 )    - ( { T = id(ID) }, id(C0, ID) )
   ]).
+
+triples(Tkn0, Ts0, Ts, S0, S) -->
+  if_(Tkn0 = open_square,
+    triples_blanknode(Tkn0, Ts0, Ts, S0, S),
+    triples_subject(Tkn0, Ts0, Ts, S0, S)
+  ).
+
+triples_subject(Tkn0, Ts0, Ts, S0, S) -->
+  subject(Sub, Tkn0, Ts0, Ts1, S0, S1),
+  token(Tkn1),
+  predicate_list(Sub, Tkn1, Ts1, Ts, S1, S).
+
+triples_blanknode(Tkn0, Ts0, Ts, S0, S) -->
+  blank_node_properties(Sub, Tkn0, Ts0, Ts1, S0, S1),
+  token(Tkn1),
+  if_(Tkn1 = dot,
+    { Ts = Ts1, S = S1 },
+    predicate_list(Sub, Tkn1, Ts1, Ts, S1, S)
+  ).
+
+predicate_list(Sub, Tkn0, Ts0, Ts, S0, S) -->
+  verb(Verb, Tkn0, Ts0, Ts1, S0, S1),
+  token(Tkn1),
+  object_list(Sub, Verb, Tkn1, Ts1, Ts2, S1, S2),
+  token(Tkn2),
+  matcheq_expect(Tkn2, predicate_list, [
+    semi  - ( token(Tkn3), predicate_list(Sub, Tkn3, Ts2, Ts, S2, S) ),
+    dot   - { Ts = Ts2, S = S2 }
+  ]).
+
+object_list(Sub, Verb, Tkn0, Ts0, Ts, S0, S) -->
+  object(Obj, Tkn0, Ts0, Ts1, S0, S1),
+  { Ts1 = [t(Sub, Verb, Obj) | Ts2 ] },
+  token(Tkn1),
+  matcheq_expect(Tkn1, object_list, [
+    comma - ( token(Tkn2), object_list(Sub, Verb, Tkn2, Ts2, Ts, S1, S) ),
+    dot   - { Ts = Ts2, S = S1 }
+  ]).
+
+blank_node_properties(X, Tkn0, Ts0, Ts, S0, S) -->
+  { Tkn0 = open_square }, % TODO: remove this check
+  { gen_blank_node(X, S0, S1) },
+  token(Tkn1),
+  predicate_list(X, Tkn1, Ts0, Ts, S1, S).
+
+% TODO: do token analisis
+subject(Sub, Tkn0, Ts0, Ts, S0, S) -->
+  ( { Ts = Ts0 }, iri(Sub, Tkn0, S0, S)
+  ; { Ts = Ts0 }, blank_node(Sub, Tkn0, S0, S)
+  ; collection(Sub, Tkn0, Ts0, Ts, S0, S)
+  ).
+
+predicate(Pred, Tkn0, S0, S) -->
+  if_(Tkn0 = a,
+    { Pred = a },
+    iri(Pred, Tkn0, S0, S)
+  ).
+
+% TODO: do token analisis
+object(Obj, Tkn0, Ts0, Ts, S0, S) -->
+  ( { Ts = Ts0 }, iri(Obj, Tkn0, S0, S)
+  ; { Ts = Ts0 }, blank_node(Obj, Tkn0, S0, S)
+  ; collection(Obj, Tkn0, Ts0, Ts, S0, S)
+  ; blank_node_properties(Obj, Tkn0, Ts0, Ts, S0, S)
+  ; { Ts = Ts0 }, literal(Obj, Tkn0, S0, S)
+  ).
+
+iri(X, Tkn0, S0, S) -->
+  % TODO
+[].
+blank_node(X, Tkn0, S0, S) -->
+  % TODO
+[].
+collection(X, Tkn0, Ts0, Ts, S0, S) -->
+  % TODO
+[].
+literal(X, Tkn0, S0, S) -->
+  % TODO
+[].
 
 simple_triple(Subj, Pred, Obj) -->
   node(sub, Subj), ws_, node(pred, Pred), ws_, node(obj, Obj).
