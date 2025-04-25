21d647b314f2e116dd89844e70e7e542c8723136
Shikiyo <shikiyo@archjk>
2025-04-25 08:54:48 -0300

    turtle: embed location inside the token

--- parsers/turtle.pl
+++ parsers/turtle.pl
@@ -1,7 +1,7 @@
 % :- module(turtle).
 
 :- use_module(library(dcgs), []).
-:- use_module(library(reif), [if_/3, (=)/3, memberd_t/3]).
+:- use_module(library(reif), [(=)/3, memberd_t/3]).
 
 :- use_module(reif_dcgs, [if_//3]).
 
@@ -10,12 +10,6 @@ matcheq(Elem, Cases) --> matcheq_impl(Cases, Elem, Cases).
 matcheq_impl([], E, Cases) --> { throw(error(unreachable_match(E, Cases))) }.
 matcheq_impl([If_2-Then | Cs], E, Cases) -->
   if_(call(If_2, E), Then, matcheq_impl(Cs, E, Cases)).
-
-matcheq_expect(Elem, PredName, Cases) --> matcheq_expect_impl(Cases, Elem, PredName, _, _).
-
-matcheq_expect_impl([], E, PredName, Expected, []) --> { throw(error(expected_got_predicate(Expected, E, PredName))) }.
-matcheq_expect_impl([C-Then | Cs], E, PredName, Expected, [C | X]) -->
-  if_(C = E, Then, matcheq_expect_impl(Cs, E, PredName, Expected, X)).
 
 :- use_module(linecol, [char//2, unchar//2]).
 
@@ -39,24 +33,33 @@ comment(Comment) -->
     )
   ).
 
-id(C0, C0) --> [].
+id(C0, L0, tkn(L0, id(C0))) --> [].
 
 token(T) -->
-  char(C0),
+  char(C0, L0),
   matcheq(C0, [
     ws_t      - token(T),
     eof_t     - { T = eof },
-    comment_t - ( { T = comment(C) }, comment(C) ),
-    comma_t   - { T = comma },
-    semi_t    - { T = semi },
-    dot_t     - { T = dot },
-    =('[')    - { T = open_square },
-    =(']')    - { T = close_square },
-    =(C0 )    - ( { T = id(ID) }, id(C0, ID) )
+    comment_t - ( { T = tkn(L0, comment(C)) }, comment(C) ),
+    comma_t   - { T = tkn(L0, comma) },
+    semi_t    - { T = tkn(L0, semi) },
+    dot_t     - { T = tkn(L0, dot) },
+    =('[')    - { T = tkn(L0, open_square) },
+    =(']')    - { T = tkn(L0, close_square) },
+    =(C0 )    - id(C0, L0, T)
   ]).
 
+if_token(tkn(_, Inner), Match_2, Then_2, Else_2) -->
+  if_(call(Match_2, Inner), Then_2, Else_2).
+
+matcheq_expect_token(tkn(Loc, Inner), PredName, Cases) --> matcheq_expect_token_impl(Cases, Inner, Loc, PredName, X, X).
+
+matcheq_expect_token_impl([], Inner, Loc, PredName, Expected, []) --> { throw(error(expected_got_at_predicate(Expected, Inner, Loc, PredName))) }.
+matcheq_expect_token_impl([C-Then | Cs], Inner, Loc, PredName, Expected, [C | X]) -->
+  if_(C = Inner, Then, matcheq_expect_token_impl(Cs, Inner, Loc, PredName, Expected, X)).
+
 triples(Tkn0, Ts0, Ts, S0, S) -->
-  if_(Tkn0 = open_square,
+  if_token(Tkn0, =(open_square),
     triples_blanknode(Tkn0, Ts0, Ts, S0, S),
     triples_subject(Tkn0, Ts0, Ts, S0, S)
   ).
@@ -69,7 +72,7 @@ triples_subject(Tkn0, Ts0, Ts, S0, S) -->
 triples_blanknode(Tkn0, Ts0, Ts, S0, S) -->
   blank_node_properties(Sub, Tkn0, Ts0, Ts1, S0, S1),
   token(Tkn1),
-  if_(Tkn1 = dot,
+  if_token(Tkn1, =(dot),
     { Ts = Ts1, S = S1 },
     predicate_list(Sub, Tkn1, Ts1, Ts, S1, S)
   ).
@@ -79,7 +82,7 @@ predicate_list(Sub, Tkn0, Ts0, Ts, S0, S) -->
   token(Tkn1),
   object_list(Sub, Verb, Tkn1, Ts1, Ts2, S1, S2),
   token(Tkn2),
-  matcheq_expect(Tkn2, predicate_list, [
+  matcheq_expect_token(Tkn2, predicate_list, [
     semi  - ( token(Tkn3), predicate_list(Sub, Tkn3, Ts2, Ts, S2, S) ),
     dot   - { Ts = Ts2, S = S2 }
   ]).
@@ -88,7 +91,7 @@ object_list(Sub, Verb, Tkn0, Ts0, Ts, S0, S) -->
   object(Obj, Tkn0, Ts0, Ts1, S0, S1),
   { Ts1 = [t(Sub, Verb, Obj) | Ts2 ] },
   token(Tkn1),
-  matcheq_expect(Tkn1, object_list, [
+  matcheq_expect_token(Tkn1, object_list, [
     comma - ( token(Tkn2), object_list(Sub, Verb, Tkn2, Ts2, Ts, S1, S) ),
     dot   - { Ts = Ts2, S = S1 }
   ]).
@@ -107,7 +110,7 @@ subject(Sub, Tkn0, Ts0, Ts, S0, S) -->
   ).
 
 predicate(Pred, Tkn0, S0, S) -->
-  if_(Tkn0 = a,
+  if_token(Tkn0, =(a),
     { Pred = a },
     iri(Pred, Tkn0, S0, S)
   ).
