33c17f7c361def655c7e8bb332fb4fc16d1ca012
Shikiyo <shikiyo@archjk>
2025-04-25 10:02:33 -0300

    turtle: add reference to specification

--- parsers/turtle.pl
+++ parsers/turtle.pl
@@ -1,3 +1,6 @@
+/*
+ * Reference: www.w3.org/TR/turtle
+**/
 % :- module(turtle).
 
 :- use_module(library(dcgs), []).
@@ -75,17 +78,32 @@ matcheq_expect_token_impl([], Inner, Loc, PredName, Expected, []) --> { throw(er
 matcheq_expect_token_impl([C-Then | Cs], Inner, Loc, PredName, Expected, [C | X]) -->
   if_(C = Inner, Then, matcheq_expect_token_impl(Cs, Inner, Loc, PredName, Expected, X)).
 
+%%%%%%%%%%%%%%%%%%%% Parser %%%%%%%%%%%%%%%%%%%%
+
+/* TODO
+ * 6.5 [1]
+ * 6.5 [2]
+ * 6.5 [3]
+ * 6.5 [4]
+ * 6.5 [5]
+ * 6.5 [5s]
+ * 6.5 [6s]
+**/
+
+/* 6.5 [6] */
 triples(Tkn0, Ts0, Ts, S0, S) -->
   if_token(Tkn0, =(open_square),
     triples_blanknode(Tkn0, Ts0, Ts, S0, S),
     triples_subject(Tkn0, Ts0, Ts, S0, S)
   ).
 
+/* 6.5 [6 : case 0] */
 triples_subject(Tkn0, Ts0, Ts, S0, S) -->
   subject(Sub, Tkn0, Ts0, Ts1, S0, S1),
   token(Tkn1),
   predicate_list(Sub, Tkn1, Ts1, Ts, S1, S).
 
+/* 6.5 [6 : case 1] */
 triples_blanknode(Tkn0, Ts0, Ts, S0, S) -->
   blank_node_properties(Sub, Tkn0, Ts0, Ts1, S0, S1),
   token(Tkn1),
@@ -94,6 +112,7 @@ triples_blanknode(Tkn0, Ts0, Ts, S0, S) -->
     predicate_list(Sub, Tkn1, Ts1, Ts, S1, S)
   ).
 
+/* 6.5 [7] */
 predicate_list(Sub, Tkn0, Ts0, Ts, S0, S) -->
   verb(Verb, Tkn0, Ts0, Ts1, S0, S1),
   token(Tkn1),
@@ -104,6 +123,7 @@ predicate_list(Sub, Tkn0, Ts0, Ts, S0, S) -->
     dot   - { Ts = Ts2, S = S2 }
   ]).
 
+/* 6.5 [8] */
 object_list(Sub, Verb, Tkn0, Ts0, Ts, S0, S) -->
   object(Obj, Tkn0, Ts0, Ts1, S0, S1),
   { Ts1 = [t(Sub, Verb, Obj) | Ts2 ] },
@@ -113,12 +133,16 @@ object_list(Sub, Verb, Tkn0, Ts0, Ts, S0, S) -->
     dot   - { Ts = Ts2, S = S1 }
   ]).
 
-blank_node_properties(X, Tkn0, Ts0, Ts, S0, S) -->
-  { Tkn0 = open_square }, % TODO: remove this check
-  { gen_blank_node(X, S0, S1) },
-  token(Tkn1),
-  predicate_list(X, Tkn1, Ts0, Ts, S1, S).
+/* 6.5 [9] */
+verb(Pred, Tkn0, S0, S) -->
+  if_token(Tkn0, =(id(a)),
+    /* 6.5 [9 case 1] */
+    { Pred = a },
+    /* 6.5 [11] (inlined) [9 case 0] */
+    iri(Pred, Tkn0, S0, S)
+  ).
 
+/* 6.5 [10] */
 % TODO: do token analisis
 subject(Sub, Tkn0, Ts0, Ts, S0, S) -->
   ( { Ts = Ts0 }, iri(Sub, Tkn0, S0, S)
@@ -126,41 +150,57 @@ subject(Sub, Tkn0, Ts0, Ts, S0, S) -->
   ; collection(Sub, Tkn0, Ts0, Ts, S0, S)
   ).
 
-predicate(Pred, Tkn0, S0, S) -->
-  if_token(Tkn0, =(a),
-    { Pred = a },
-    iri(Pred, Tkn0, S0, S)
-  ).
-
+/* 6.5 [12] */
 % TODO: do token analisis
 object(Obj, Tkn0, Ts0, Ts, S0, S) -->
+  /* 6.5 [12 case 0] */
   ( { Ts = Ts0 }, iri(Obj, Tkn0, S0, S)
+  /* 6.5 [12 case 1] */
   ; { Ts = Ts0 }, blank_node(Obj, Tkn0, S0, S)
+  /* 6.5 [12 case 2] */
   ; collection(Obj, Tkn0, Ts0, Ts, S0, S)
+  /* 6.5 [12 case 3] */
   ; blank_node_properties(Obj, Tkn0, Ts0, Ts, S0, S)
+  /* 6.5 [12 case 4] */
   ; { Ts = Ts0 }, literal(Obj, Tkn0, S0, S)
   ).
 
-iri(X, Tkn0, S0, S) -->
+/* 6.5 [13] */
+literal(X, Tkn0, Tkn) -->
+  matcheq_expect_token(Tkn0, literal, [
+    /* 6.5 [13 case 0] */
+    string(Str) - ( token(Tkn1), rdf_literal(X, Str, Tkn1, Tkn) ),
+    /* 6.5 [16] (inlined) [13 case 1] */
+    number(N)   - ( { X = literal(number, N) }, token(Tkn) )
+    /* 6.5 [133s] (inlined) [13 case 2] */
+    boolean(B)  - ( { X = literal(boolean, B) }, token(Tkn) )
+  ]).
+
+/* 6.5 [14] */
+blank_node_properties(X, Tkn0, Ts0, Ts, S0, S) -->
+  { Tkn0 = open_square }, % TODO: remove this check
+  { gen_blank_node(X, S0, S1) },
+  token(Tkn1),
+  predicate_list(X, Tkn1, Ts0, Ts, S1, S).
+
+/* 6.5 [15] */
+collection(X, Tkn0, Tkn, Ts0, Ts, S0, S) -->
   % TODO
 [].
+
+/* 6.5 [128s] */
+rdf_literal(X, Str, Tkn0, Tkn) -->
+  /* 6.5 [17] (inlined) */
+  % TODO
+[].
+
+/* 6.5 [135s] */
+iri(X, Tkn0, S0, S) -->
+  /* 6.5 [136s] (inlined) */
+  % TODO
+[].
+
+/* 6.5 [137s] */
 blank_node(X, Tkn0, S0, S) -->
   % TODO
 [].
-collection(X, Tkn0, Ts0, Ts, S0, S) -->
-  % TODO
-[].
-literal(X, Tkn0, S0, S) -->
-  % TODO
-[].
-
-simple_triple(Subj, Pred, Obj) -->
-  node(sub, Subj), ws_, node(pred, Pred), ws_, node(obj, Obj).
-
-node(Kind, A, S0, S) -->
-  ( { S = S0 }, iri(A)
-  ; { S = S0 }, literal(A)
-  ; { S = S0, state_prefix(S, SP) }, prefixed(SP, A)
-  ; { state_blank(S0, S, SB0, SB) }, blank_node(A, SB0, SB) % Kind != pred
-  ; { S = S0 }, collection(A)
-  ).
