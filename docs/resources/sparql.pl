d311d64a2f81fdefa2b9310d844af5d4bded2c18
Shikiyo <shikiyo@archjk>
2025-07-07 17:00:41 -0300

    Add SICStus optional to sparql.pl

--- docs/resources/sparql.pl
+++ docs/resources/sparql.pl
@@ -1,10 +1,14 @@
-% NOTE: if the implementation supports soft-cut (*->)/2
-% it could be implemented as
+% NOTE: if the prolog implementation supports "soft cut"
+% optional could be implemented with it
+%
+% For example SWI-Prolog supports "soft cut" (*->)/2
+% (docs) https://www.swi-prolog.org/pldoc/doc_for?object=(*-%3E)/2
 %
 optional(Sub, Pred, Obj) :- ( rdf(Sub, Pred, Obj) *-> true ; false ).
 %
-% For example SWI-Prolog supports it
-% (documentation) https://www.swi-prolog.org/pldoc/doc_for?object=(*-%3E)/2
+% For example SICStus-Prolog supports "soft cut" if/3
+% (docs) https://sicstus.sics.se/sicstus/docs/4.1.2/html/sicstus/ref_002dsem_002dctr_002dite.html
+optional(Sub, Pred, Obj) :- if(rdf(Sub, Pred, Obj), true, false).
 %
 % otherwise
 %
